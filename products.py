cac4fb51596e9ce447d5575a831621615f5683a1
gwitfeng <gwitfeng@gmail.com>
2018-09-17 23:28:44 +0800

    add check file function

--- products.py
+++ products.py
@@ -1,13 +1,20 @@
-# products
+import os # operating system
 products = []
-# 讀取檔案
-with open ('products.csv', 'r',encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name,price])
-print(products)        
+
+if os.path.isfile('products.csv'): # 檢察檔案
+    print('yeah ! 找到檔案了')
+    # 讀取檔案 
+    with open ('products.csv', 'r',encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name,price])
+    print(products) 
+
+else:
+    print('找不到檔案')
+
 
 # 輸入產品資料
 while True:
@@ -16,11 +23,11 @@ while True:
         break
     price = input('請輸入產品價格 ')
     products.append([name, price]) 
- print(products)
+print(products)
 
 # 印出產品資料
- for p in products:
-     print(p[0] ,'的價格是 ', p[1])
+for p in products:
+    print(p[0] ,'的價格是 ', p[1])
 
 # 寫入檔案
 with open('products.csv' , 'w' , encoding = 'utf-8') as f:
